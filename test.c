44a985905f0552c6951dcb11a26d1f2ab2b49b8e
iluvshiwoon <sntcillian@gmail.com>
2024-02-27 12:27:09 +0100

    red img

--- test.c
+++ test.c
@@ -6,7 +6,7 @@
 /*   By: kgriset <kgriset@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/25 10:36:49 by kgriset           #+#    #+#             */
-/*   Updated: 2024/02/27 12:15:00 by kgriset          ###   ########.fr       */
+/*   Updated: 2024/02/27 12:26:56 by kgriset          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,18 +20,35 @@ void my_mlx_put_pixel(t_data *data, int x, int y, int color)
     *(unsigned int*)dst = color;
 }
 
+int create_trgb(int t, int r, int g, int b)
+{
+    return (t << 24 | r << 16 | g << 8 | b);
+}
+
 int main(void)
 {
     void	*mlx;
 	void	*mlx_win;
     t_data img;
+    int i;
+    int j;
 
 	mlx = mlx_init();
 
 	mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello world!");
     img.img = mlx_new_image(mlx, 1920, 1080);
     img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length, &img.endian);
-    my_mlx_put_pixel(&img, 5, 5, 0x00FF0000);
+    i = 0;
+    while (i < 1920)
+    {
+        j = 0;
+        while (j < 1080)
+        {
+            my_mlx_put_pixel(&img, 5, 5, create_trgb(0,255,0,0));
+            j++;
+        }
+        i++;
+    }
     mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
 	mlx_loop(mlx);
 }
