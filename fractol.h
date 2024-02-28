2175ff7c01c8c513224339e1658905107d9e3b05
iluvshiwoon <sntcillian@gmail.com>
2024-02-28 19:30:03 +0100

    red img

--- fractol.h
+++ fractol.h
@@ -6,13 +6,14 @@
 /*   By: kgriset <kgriset@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/27 12:03:53 by kgriset           #+#    #+#             */
-/*   Updated: 2024/02/28 18:46:09 by kgriset          ###   ########.fr       */
+/*   Updated: 2024/02/28 19:22:45 by kgriset          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FRACTOL_H
 #include "minilibx-linux/mlx.h"
 #define XK_MISCELLANY
+#define XK_LATIN1
 #include <X11/keysymdef.h>
 # define FRACTOL_H
 # include "42_MyLibC/mylibc.h"
--- test.c
+++ test.c
@@ -6,11 +6,18 @@
 /*   By: kgriset <kgriset@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/25 10:36:49 by kgriset           #+#    #+#             */
-/*   Updated: 2024/02/28 19:14:20 by kgriset          ###   ########.fr       */
+/*   Updated: 2024/02/28 19:30:01 by kgriset          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fractol.h"
+
+typedef struct s_pos {
+    void * mlx;
+    void * win;
+    int a;
+    int b;
+} t_pos;
 
 void my_mlx_put_pixel(t_data *data, int x, int y, int color)
 {
@@ -34,16 +41,53 @@ int close_win (int keycode, t_vars *vars)
     return (0);
 }
 
+int change_pos (int keycode, t_pos * pos)
+{
+    if (keycode == XK_w)
+        pos->a += 1;
+    return 1;
+}
+
+int render_next_frame(t_pos * pos)
+{
+    int i;
+    int j;
+    t_data img;
+
+    mlx_hook(pos->win, 2, 1L<<0, &change_pos, pos);
+    img.img = mlx_new_image(pos->mlx, 1920, 1080);
+    img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length, &img.endian);
+    i = 0;
+    while (i < 1000)
+    {
+        j = 0;
+        while (j < 1000)
+        {
+            if (pow((double)i - pos->a,2) + pow((double)j - pos->b,2) < 500)
+                my_mlx_put_pixel(&img, i, j, create_trgb(40,255,0+i%255,0));
+            j++;
+        }
+        i++;
+    }
+    return 1;
+}
+
 int main(void)
 {
     t_vars vars;
     t_data img;
+    t_pos * pos;
     int i;
     int j;
 
+    pos = malloc(sizeof(*pos));
+    pos->a = 200;
+    pos->b = 200;
 	vars.mlx = mlx_init();
+    pos->mlx = vars.mlx;
 
 	vars.win = mlx_new_window(vars.mlx, 1920, 1080, "test!");
+    pos->win = vars.win;
     img.img = mlx_new_image(vars.mlx, 1920, 1080);
     img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length, &img.endian);
     i = 0;
@@ -59,6 +103,7 @@ int main(void)
         i++;
     }
     mlx_put_image_to_window(vars.mlx, vars.win, img.img, 400, 400);
+    mlx_loop_hook(vars.mlx, &render_next_frame, pos);
     mlx_hook(vars.win, 2, 1L<<0, &close_win, &vars);
 	mlx_loop(vars.mlx);
 }
