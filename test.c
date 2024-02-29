d37f9fc2feb6dfb22bd2895de7c3b39fda63c218
iluvshiwoon <sntcillian@gmail.com>
2024-02-29 18:03:08 +0100

    red img

--- test.c
+++ test.c
@@ -6,7 +6,7 @@
 /*   By: kgriset <kgriset@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/25 10:36:49 by kgriset           #+#    #+#             */
-/*   Updated: 2024/02/29 18:02:17 by kgriset          ###   ########.fr       */
+/*   Updated: 2024/02/29 18:03:05 by kgriset          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,36 +42,36 @@ int calc_mandelbrot(t_vars *vars) {
   img.img = mlx_new_image(vars->mlx, 1920, 1080);
   img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
                                &img.endian);
-  // while (vars->p_x < 1920)
-  // {
-  //     vars->p_y = 0;
-  //     vars->y = -1080./200;
-  //     while (vars->p_y < 1080)
-  //     {
-  //         i = 0;
-  //         zr = 0.0;
-  //         zi = 0.0;
-  //         while (i < 50)
-  //         {
-  //             zr_temp = zr*zr - zi*zi + vars->x;
-  //             zi = 2*zr*zi + vars->y;
-  //             zr = zr_temp;
-  //             if (zr*zr + zi*zi >= 4)
-  //                 break;
-  //             ++i;
-  //         }
-  //         if (i == 50)
-  //             my_mlx_put_pixel(&img, vars->p_x, vars->p_y,
-  //             create_trgb(0,0,0,0));
-  //         else
-  //             my_mlx_put_pixel(&img, vars->p_x, vars->p_y,
-  //             create_trgb(0,(1*i)%255,(2*i)%255,(3*i)%255));
-  //         ++(vars->p_y);
-  //         vars->y += 0.01;
-  //     }
-  //     ++(vars->p_x);
-  //     vars->x += 0.01;
-  // }
+  while (vars->p_x < 1920)
+  {
+      vars->p_y = 0;
+      vars->y = -1080./200;
+      while (vars->p_y < 1080)
+      {
+          i = 0;
+          zr = 0.0;
+          zi = 0.0;
+          while (i < 50)
+          {
+              zr_temp = zr*zr - zi*zi + vars->x;
+              zi = 2*zr*zi + vars->y;
+              zr = zr_temp;
+              if (zr*zr + zi*zi >= 4)
+                  break;
+              ++i;
+          }
+          if (i == 50)
+              my_mlx_put_pixel(&img, vars->p_x, vars->p_y,
+              create_trgb(0,0,0,0));
+          else
+              my_mlx_put_pixel(&img, vars->p_x, vars->p_y,
+              create_trgb(0,(1*i)%255,(2*i)%255,(3*i)%255));
+          ++(vars->p_y);
+          vars->y += 0.01;
+      }
+      ++(vars->p_x);
+      vars->x += 0.01;
+  }
   mlx_put_image_to_window(vars->mlx, vars->win, img.img, 0, 0);
   return 1;
 }
