1b58fac2a3b07d47eefea4ad8a41fe901947b9a4
iluvshiwoon <sntcillian@gmail.com>
2024-02-29 18:23:32 +0100

    red img

--- test.c
+++ test.c
@@ -6,7 +6,7 @@
 /*   By: kgriset <kgriset@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/25 10:36:49 by kgriset           #+#    #+#             */
-/*   Updated: 2024/02/29 18:22:51 by kgriset          ###   ########.fr       */
+/*   Updated: 2024/02/29 18:23:30 by kgriset          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -60,7 +60,7 @@ int calc_mandelbrot(t_vars *vars) {
                   break;
               ++i;
           }
-          if (i == 50)
+          if (i == 10)
               my_mlx_put_pixel(&img, vars->p_x, vars->p_y,
               create_trgb(0,0,0,0));
           else
