106ff9765a245d51fae414699add4e7c2529be04
iluvshiwoon <sntcillian@gmail.com>
2024-03-01 15:29:57 +0100

    red img

--- test.c
+++ test.c
@@ -6,7 +6,7 @@
 /*   By: kgriset <kgriset@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/25 10:36:49 by kgriset           #+#    #+#             */
-/*   Updated: 2024/03/01 15:28:56 by kgriset          ###   ########.fr       */
+/*   Updated: 2024/03/01 15:29:55 by kgriset          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,13 +29,13 @@ int key_events(int keycode, t_vars *vars) {
     mlx_destroy_window(vars->mlx, vars->win);
     exit(0);
   } else if (keycode == 100)
-    vars->offset_x -= 1;
+    vars->offset_x -= 0.5;
   else if (keycode == 97)
-    vars->offset_x += 1; 
+    vars->offset_x += 0.5; 
   else if (keycode == 119)
-    vars->offset_y += 1;
+    vars->offset_y += 0.5;
   else if (keycode == 115)
-    vars->offset_y -= 1;
+    vars->offset_y -= 0.5;
   else
     return (0);
   calc_mandelbrot(vars);
