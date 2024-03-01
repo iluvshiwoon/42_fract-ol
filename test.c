a13c790485ef46fd6da8c2e8f20c8c1acc5ec546
iluvshiwoon <sntcillian@gmail.com>
2024-03-01 19:07:12 +0100

    red img

--- test.c
+++ test.c
@@ -6,7 +6,7 @@
 /*   By: kgriset <kgriset@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/25 10:36:49 by kgriset           #+#    #+#             */
-/*   Updated: 2024/03/01 19:05:54 by kgriset          ###   ########.fr       */
+/*   Updated: 2024/03/01 19:07:10 by kgriset          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,15 +29,15 @@ int key_events(int keycode, t_vars *vars) {
     mlx_destroy_window(vars->mlx, vars->win);
     exit(0);
   } else if (keycode == KEY_D)
-    vars->offset_x += 2. / vars->zoom*10;
+    vars->offset_x += 2. / vars->zoom * 10;
   else if (keycode == KEY_A)
-    vars->offset_x -= 2. / vars->zoom*10;
+    vars->offset_x -= 2. / vars->zoom * 10;
   else if (keycode == KEY_W)
-    vars->offset_y -= 2. / vars->zoom*10;
+    vars->offset_y -= 2. / vars->zoom * 10;
   else if (keycode == KEY_S)
-    vars->offset_y += 2. / vars->zoom*10;
+    vars->offset_y += 2. / vars->zoom * 10;
   else if (keycode == KEY_PLUS)
-            vars->zoom += 500;
+    vars->zoom += 500;
   else if (keycode == KEY_MINUS)
     vars->zoom -= 500;
 
@@ -47,13 +47,14 @@ int key_events(int keycode, t_vars *vars) {
   return 1;
 }
 
-double scale(char axe, double x, t_vars * vars)
-{
-    if (axe == 'w')
-        return(((vars->max_r - vars->min_r) * (x - 0))/(vars->view_width - 0) + vars->min_r);
-    else if (axe == 'h')
-        return(((vars->max_i - vars->min_i) * (x - 0))/(vars->view_height - 0) + vars->min_i);
-    return 0;
+double scale(char axe, double x, t_vars *vars) {
+  if (axe == 'w')
+    return (((vars->max_r - vars->min_r) * (x - 0)) / (vars->view_width - 0) +
+            vars->min_r);
+  else if (axe == 'h')
+    return (((vars->max_i - vars->min_i) * (x - 0)) / (vars->view_height - 0) +
+            vars->min_i);
+  return 0;
 }
 
 int calc_mandelbrot(t_vars *vars) {
@@ -65,7 +66,7 @@ int calc_mandelbrot(t_vars *vars) {
 
   vars->p_x = 0;
   // vars->x = -VW / vars->zoom + vars->offset_x;
-    vars->x = scale('w',0.,vars);
+  vars->x = scale('w', 0., vars);
 
   img.img = mlx_new_image(vars->mlx, VW, VH);
   img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
@@ -73,7 +74,7 @@ int calc_mandelbrot(t_vars *vars) {
   while (vars->p_x < VW) {
     vars->p_y = 0;
     // vars->y = -VH / vars->zoom + vars->offset_y;
-        vars->y = scale('h',0.,vars);
+    vars->y = scale('h', 0., vars);
     while (vars->p_y < VH) {
       i = 0;
       zr = 0.0;
@@ -94,11 +95,11 @@ int calc_mandelbrot(t_vars *vars) {
             create_trgb(0, (10 * i) % 255, (20 * i) % 255, (30 * i) % 255));
       ++(vars->p_y);
       // vars->y += 2. / vars->zoom;
-            vars->y += scale('h',1.,vars) - scale('h',0.,vars);
+      vars->y += scale('h', 1., vars) - scale('h', 0., vars);
     }
     ++(vars->p_x);
     // vars->x += 2. / vars->zoom;
-        vars->x += scale('w',1.,vars) - scale('w',0.,vars);
+    vars->x += scale('w', 1., vars) - scale('w', 0., vars);
   }
   mlx_put_image_to_window(vars->mlx, vars->win, img.img, 0, 0);
   return 1;
@@ -107,25 +108,26 @@ int calc_mandelbrot(t_vars *vars) {
 int main(void) {
   t_vars *vars;
 
-    vars->view_width = 1920 / 2;
-    vars->view_height = 1080 /2;
   vars = malloc(sizeof(*vars));
+  vars->view_width = 1920 / 2;
+  vars->view_height = 1080 / 2;
   vars->mlx = mlx_init();
-  vars->win = mlx_new_window(vars->mlx, vars->view_width, vars->view_height, "Mandelbrot");
-    vars->min_r = -4.0;
-    vars->max_r = 4.0;
-    vars->min_i = -4.0;
-    vars->max_i = 4.0;
+  vars->win = mlx_new_window(vars->mlx, vars->view_width, vars->view_height,
+                             "Mandelbrot");
+  vars->min_r = -4.0;
+  vars->max_r = 4.0;
+  vars->min_i = -4.0;
+  vars->max_i = 4.0;
   vars->zoom = 200;
   vars->offset_x = 0;
   vars->offset_y = 0;
-    printf("%f\n", scale('w',0.,vars));
-    printf("%f\n", scale('w',1.,vars));
-    printf("%f\n", scale('w',1920.,vars));
+  printf("%f\n", scale('w', 0., vars));
+  printf("%f\n", scale('w', 1., vars));
+  printf("%f\n", scale('w', 1920., vars));
 
-    printf("%f\n", scale('h',0.,vars));
-    printf("%f\n", scale('h',1.,vars));
-    printf("%f\n", scale('h',1920.,vars));
+  printf("%f\n", scale('h', 0., vars));
+  printf("%f\n", scale('h', 1., vars));
+  printf("%f\n", scale('h', 1920., vars));
 
   calc_mandelbrot(vars);
   mlx_key_hook(vars->win, &key_events, vars);
