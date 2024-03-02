7a868317d5bed13bdb35b2c0b6a18552601ca7ac
iluvshiwoon <sntcillian@gmail.com>
2024-03-02 15:59:28 +0100

    red img

--- test.c
+++ test.c
@@ -6,13 +6,14 @@
 /*   By: kgriset <kgriset@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/25 10:36:49 by kgriset           #+#    #+#             */
-/*   Updated: 2024/03/02 15:54:44 by kgriset          ###   ########.fr       */
+/*   Updated: 2024/03/02 15:59:27 by kgriset          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fractol.h"
 #include "keycode.h"
 
+#include <stdio.h>
 void my_mlx_put_pixel(t_data *data, int x, int y, int color) {
   char *dst;
   dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
@@ -23,63 +24,56 @@ int create_trgb(int t, int r, int g, int b) {
   return (t << 24 | r << 16 | g << 8 | b);
 }
 
-int key_released(int keycode, t_vars * vars)
-{
+int key_released(int keycode, t_vars *vars) {
   vars->is_pressed = 0;
   if (keycode == KEY_ESC) {
     mlx_destroy_window(vars->mlx, vars->win);
     exit(0);
   } else if (keycode == KEY_D)
-        vars->is_pressed = 0;
+    vars->is_pressed = 0;
+  return 1;
+}
+
+int move(t_vars *vars) {
+  if (vars->is_pressed) {
+    if (vars->direction == 'D')
+      vars->offset_x += 1;
+    else if (vars->direction == 'A')
+      vars->offset_x -= 1;
+    else if (vars->direction == 'W')
+      vars->offset_y -= 1;
+    else if (vars->direction == 'S')
+      vars->offset_y += 1;
+        printf("%f\n",vars->offset_x);
     return 1;
+  }
+  return 0;
 }
 
-int move(t_vars * vars)
-{
-    if (vars->is_pressed)
-    {
-        if (vars->direction == 'D')
-            vars->offset_x += scale('w', 1.,vars);
-        else if (vars->direction == 'A')
-            vars->offset_x -= scale('w', 1.,vars);
-        else if (vars->direction == 'W')
-            vars->offset_y -= scale('w', 1.,vars);
-        else if (vars->direction == 'S')
-            vars->offset_y += scale('w', 1.,vars);
-        return 1;
-    }
-    return 0;
-
+void set_key_pressed(t_vars *vars, char direction) {
+  vars->is_pressed = 1;
+  vars->direction = direction;
 }
 
-void set_key_pressed(t_vars * vars, char direction)
-{
-    vars->is_pressed = 1;
-    vars->direction = direction;
-}
-
-#include <stdio.h>
 int key_events(int keycode, t_vars *vars) {
   printf("%d\n", keycode);
   if (keycode == KEY_ESC) {
     mlx_destroy_window(vars->mlx, vars->win);
     exit(0);
   } else if (keycode == KEY_D)
-        set_key_pressed(vars, 'D');
+    set_key_pressed(vars, 'D');
   else if (keycode == KEY_A)
-        set_key_pressed(vars, 'A');
+    set_key_pressed(vars, 'A');
   else if (keycode == KEY_W)
-        set_key_pressed(vars, 'W');
+    set_key_pressed(vars, 'W');
   else if (keycode == KEY_S)
-        set_key_pressed(vars, 'S');
+    set_key_pressed(vars, 'S');
   else if (keycode == KEY_PLUS)
     vars->zoom += 500;
   else if (keycode == KEY_MINUS)
     vars->zoom -= 500;
-
   else
     return (0);
-  calc_mandelbrot(vars);
   return 1;
 }
 
@@ -98,11 +92,10 @@ double scale(char axe, double x, t_vars *vars) {
   return 0;
 }
 
-int render(t_vars * vars)
-{
-    if (move(vars))
-        calc_mandelbrot(vars);
-    return 1;
+int render(t_vars *vars) {
+  if (move(vars))
+    calc_mandelbrot(vars);
+  return 1;
 }
 
 int calc_mandelbrot(t_vars *vars) {
@@ -113,14 +106,14 @@ int calc_mandelbrot(t_vars *vars) {
   double zr_temp;
 
   vars->p_x = 0;
-  vars->x = scale('w', 0., vars) + vars->offset_x;
+  vars->x = scale('w', 0. + vars->offset_x, vars);
 
   img.img = mlx_new_image(vars->mlx, VW, VH);
   img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
                                &img.endian);
   while (vars->p_x < VW) {
     vars->p_y = 0;
-    vars->y = scale('h', 0., vars) + vars->offset_y;
+    vars->y = scale('h', 0. + vars->offset_y, vars);
     while (vars->p_y < VH) {
       i = 0;
       zr = 0.0;
@@ -173,7 +166,7 @@ int main(void) {
   calc_mandelbrot(vars);
   mlx_hook(vars->win, ON_KEYDOWN, (1L << 0), &key_events, vars);
   mlx_hook(vars->win, ON_KEYUP, (1L << 1), &key_released, vars);
-    mlx_loop_hook(vars->win, &render, vars);
+  mlx_loop_hook(vars->win, &render, vars);
   mlx_hook(vars->win, ON_DESTROY, 0, &close_win, vars);
   mlx_loop(vars->mlx);
 }
