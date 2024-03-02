c4d07709504d16944133fb1691069d233e1d28bf
iluvshiwoon <sntcillian@gmail.com>
2024-03-02 16:52:40 +0100

    red img

--- test.c
+++ test.c
@@ -6,7 +6,7 @@
 /*   By: kgriset <kgriset@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/25 10:36:49 by kgriset           #+#    #+#             */
-/*   Updated: 2024/03/02 16:38:00 by kgriset          ###   ########.fr       */
+/*   Updated: 2024/03/02 16:52:38 by kgriset          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -167,6 +167,7 @@ int main(void) {
   printf("%f\n", scale('h', vars->view_height, vars));
 
   vars->mlx = mlx_init();
+    mlx_do_key_autorepeatoff(vars->mlx);
   vars->win = mlx_new_window(vars->mlx, vars->view_width, vars->view_height,
                              "Fract-ol");
 
