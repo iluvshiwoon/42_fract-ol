1f02cb97f7aeb23cecdfa9de502f3f14c5e16f0f
iluvshiwoon <sntcillian@gmail.com>
2024-03-05 00:24:46 +0100

    clean up

--- events.c
+++ events.c
@@ -6,7 +6,7 @@
 /*   By: kgriset <kgriset@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/25 10:36:49 by kgriset           #+#    #+#             */
-/*   Updated: 2024/03/05 00:24:11 by kgriset          ###   ########.fr       */
+/*   Updated: 2024/03/05 00:24:45 by kgriset          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,9 @@ void set_key_pressed(t_vars *vars, char direction) {
   render(vars);
 }
 
+#include <stdio.h>
 int key_events(int keycode, t_vars *vars) {
+    printf("%d\n", vars->offset_x);
   if (keycode == KEY_ESC) {
     close_win(vars);
   } else if (keycode == KEY_D || keycode == KEY_RIGHT)
@@ -37,7 +39,6 @@ int key_events(int keycode, t_vars *vars) {
   return 1;
 }
 
-#include <stdio.h>
 int mouse_events(int keycode, t_vars *vars) {
   int *x;
   int *y;
