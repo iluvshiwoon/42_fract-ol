ed1eb30a9e1365949fd30c386da6fab2f034a9e7
iluvshiwoon <sntcillian@gmail.com>
2024-03-04 19:02:49 +0100

    clean up

--- utils.c
+++ utils.c
@@ -6,7 +6,7 @@
 /*   By: kgriset <kgriset@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/04 17:19:58 by kgriset           #+#    #+#             */
-/*   Updated: 2024/03/04 19:00:34 by kgriset          ###   ########.fr       */
+/*   Updated: 2024/03/04 19:02:48 by kgriset          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "fractol.h"
@@ -14,6 +14,7 @@
 int close_win(t_vars *vars) {
   mlx_destroy_window(vars->mlx, vars->win);
   mlx_destroy_display(vars->mlx);
+  free(vars->mlx);
   free(vars);
   exit(0);
 }
