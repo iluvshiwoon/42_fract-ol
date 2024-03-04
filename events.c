786ba02e5d6d77700ff541022a94738321c03f89
iluvshiwoon <sntcillian@gmail.com>
2024-03-04 19:58:56 +0100

    clean up

--- events.c
+++ events.c
@@ -6,7 +6,7 @@
 /*   By: kgriset <kgriset@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/25 10:36:49 by kgriset           #+#    #+#             */
-/*   Updated: 2024/03/04 19:56:50 by kgriset          ###   ########.fr       */
+/*   Updated: 2024/03/04 19:58:54 by kgriset          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,18 +17,17 @@ void set_key_pressed(t_vars *vars, char direction) {
   move(vars);
   render(vars);
 }
-#include <stdio.h>
+
 int key_events(int keycode, t_vars *vars) {
-    printf("%d\n",keycode);
   if (keycode == KEY_ESC) {
     close_win(vars);
-  } else if (keycode == KEY_D)
+  } else if (keycode == KEY_D || keycode == KEY_RIGHT)
     set_key_pressed(vars, 'D');
-  else if (keycode == KEY_A)
+  else if (keycode == KEY_A || keycode == KEY_LEFT)
     set_key_pressed(vars, 'A');
-  else if (keycode == KEY_W)
+  else if (keycode == KEY_W || keycode == KEY_UP)
     set_key_pressed(vars, 'W');
-  else if (keycode == KEY_S)
+  else if (keycode == KEY_S || keycode == KEY_DOWN)
     set_key_pressed(vars, 'S');
   else if (keycode == KEY_PLUS)
     set_key_pressed(vars, '+');
