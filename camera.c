883c1ea6800f7e527afabfab4ceac2030edadee6
iluvshiwoon <sntcillian@gmail.com>
2024-03-05 00:52:34 +0100

    clean up

--- camera.c
+++ camera.c
@@ -6,7 +6,7 @@
 /*   By: kgriset <kgriset@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/04 17:16:35 by kgriset           #+#    #+#             */
-/*   Updated: 2024/03/04 17:55:05 by kgriset          ###   ########.fr       */
+/*   Updated: 2024/03/05 00:52:28 by kgriset          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "fractol.h"
@@ -25,8 +25,8 @@ void move(t_vars *vars) {
     vars->max_r = vars->max_r / vars->zoom;
     vars->min_i = vars->min_i / vars->zoom;
     vars->max_i = vars->max_i / vars->zoom;
-    vars->offset_x *= vars->zoom;
-    vars->offset_y *= vars->zoom;
+    vars->offset_x = vars->offset_x*vars->zoom;
+    vars->offset_y = vars->offset_x*vars->zoom;
   } else if (vars->direction == '-') {
     vars->offset_y /= vars->zoom;
     vars->offset_x /= vars->zoom;
