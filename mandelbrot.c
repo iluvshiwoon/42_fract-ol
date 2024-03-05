81b4b842691dda3368d9bc3cdf6fc86410d3423e
iluvshiwoon <sntcillian@gmail.com>
2024-03-06 00:33:56 +0100

    clean up

--- mandelbrot.c
+++ mandelbrot.c
@@ -6,7 +6,7 @@
 /*   By: kgriset <kgriset@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/04 17:14:42 by kgriset           #+#    #+#             */
-/*   Updated: 2024/03/06 00:32:17 by kgriset          ###   ########.fr       */
+/*   Updated: 2024/03/06 00:33:54 by kgriset          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "fractol.h"
@@ -36,7 +36,9 @@ t_color *get_color(double i, t_color *gradient, t_color *color) {
   int i_floor;
   i_floor = floor(i);
   if (i_floor == PASS)
-        --i_floor;
+    --i_floor;
+  else if (i_floor < 0)
+    i_floor = 0;
   i_scaled = (double)i / PASS;
 
   color->transparency = gradient[i_floor].transparency +
