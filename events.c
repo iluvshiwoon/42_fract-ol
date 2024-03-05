053a2b9ae26516280c3e566fba2098c35e032470
iluvshiwoon <sntcillian@gmail.com>
2024-03-05 01:03:22 +0100

    clean up

--- events.c
+++ events.c
@@ -6,7 +6,7 @@
 /*   By: kgriset <kgriset@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/25 10:36:49 by kgriset           #+#    #+#             */
-/*   Updated: 2024/03/05 01:02:09 by kgriset          ###   ########.fr       */
+/*   Updated: 2024/03/05 01:03:21 by kgriset          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ void set_key_pressed(t_vars *vars, char direction) {
 
 #include <stdio.h>
 int key_events(int keycode, t_vars *vars) {
-    printf("%f\n%f\n",vars->offset_x, vars->x);
+    printf("\n--------------\n%f\n%f\n",vars->offset_x, vars->x);
   if (keycode == KEY_ESC) {
     close_win(vars);
   } else if (keycode == KEY_D || keycode == KEY_RIGHT)
