db288791d92c45dc37cbac18ebbe324ee1cd33a5
iluvshiwoon <sntcillian@gmail.com>
2024-03-07 15:58:42 +0100

    clean up

--- fractol.c
+++ fractol.c
@@ -6,7 +6,7 @@
 /*   By: kgriset <kgriset@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/04 17:08:57 by kgriset           #+#    #+#             */
-/*   Updated: 2024/03/07 15:48:04 by kgriset          ###   ########.fr       */
+/*   Updated: 2024/03/07 15:58:27 by kgriset          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "fractol.h"
@@ -27,6 +27,11 @@ static void init(t_vars *vars) {
   vars->mouse_x = 0;
   vars->mouse_y = 0;
 }
+static void print_title(void) {
+  ft_printf("________/"
+            "\\\\\____________________________________________________________/"
+            "\\\\________________/\\\\\\____\n");
+}
 
 static void parse_input(t_vars *vars, int argc, char **argv) {
   int status;
@@ -38,6 +43,7 @@ static void parse_input(t_vars *vars, int argc, char **argv) {
     else if (argv[1][0] == 'J')
       vars->type = 'J';
   } else {
+    print_title();
     ft_printf("Usage: ./fractol <set> <options>\nSets: 'M' Mandelbrot\n'B' "
               "Burning Ship\n'J' Julia\nFor Julia's sets you can specify a "
               "complex in the range (0, 1 + i)\n");
